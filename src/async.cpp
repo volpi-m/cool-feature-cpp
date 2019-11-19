3fe5bc240eee543cabdfa102812dcc0b765f3e88
Lucas <lucas.tabis@epitech.eu>
2019-11-19 20:59:36 +0100

    [minor] comments : Deleted unused comments.

--- src/async.cpp
+++ src/async.cpp
@@ -14,34 +14,6 @@
 #include <chrono>
 #include <mutex>
 
-//static std::mutex mutexVar;
-
-// std::vector<std::string> getFileNamesFromDirectory(const std::string &directory)
-// {
-//     DIR *dir = nullptr;
-//     struct dirent *ent = nullptr;
-//     std::vector<std::string> v;
-
-//     if ((dir = opendir(directory.c_str())) != NULL) {
-//         while ((ent = readdir(dir)) != NULL) {
-//             std::string str = (ent->d_name);
-//             v.push_back(str);
-//         }
-//         closedir(dir);
-//     }
-//     return v;
-// }
-
-// void openFile(std::vector<std::ofstream *> vStream, std::string filePath)
-// {
-//     std::ofstream file(filePath);
-
-//     if (file.is_open()) {
-//         std::lock_guard<std::mutex> lock(mutexVar);
-//         vStream.push_back(&file);
-//     }
-// }
-
 void takeYourTime()
 {
     std::this_thread::sleep_for(std::chrono::seconds(1));
